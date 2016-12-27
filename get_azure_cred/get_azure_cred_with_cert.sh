4051a1d7c6de1093c6b0be51c4f47c3a606b8ee9
Dale Wickizer <daleewickizer@gmail.com>
2016-12-27 07:33:17 +0000

    Finished get_azure_cred.sh. Also have version that uses certificate.

--- get_azure_cred/get_azure_cred_with_cert.sh
+++ get_azure_cred/get_azure_cred_with_cert.sh
@@ -15,20 +15,20 @@
 # Initialize Parameters
 # Create hidden directory for stuff if it doesn't exist
 
-if [ ! -d $HOME/.Azure ]; then
-    mkdir $HOME/.Azure
+if [ ! -d ~/.Azure ]; then
+    mkdir ~/.Azure
 fi
 
 # Use separate log file for each important step.
 
-AzureCliInstallLog=$HOME/.Azure/AzureCliInstallLog
-AzureAccountLog=$HOME/.Azure/PMCAzureAccountLog
-AzureAppLog=$HOME/.Azure/PMCAzureAppLog
-AzureServicePrincipalLog=$HOME/.Azure/PMCAzureServicePrincipalLog
-AzureRoleLog=$HOME/.Azure/PMCAzureRoleLog
-AzureRoleMapLog=$HOME/.Azure/PMCAzureRoleMapLog
+AzureCliInstallLog=~/.Azure/AzureCliInstallLog
+AzureAccountLog=~/.Azure/PMCAzureAccountLog
+AzureAppLog=~/.Azure/PMCAzureAppLog
+AzureServicePrincipalLog=~/.Azure/PMCAzureServicePrincipalLog
+AzureRoleLog=~/.Azure/PMCAzureRoleLog
+AzureRoleMapLog=~/.Azure/PMCAzureRoleMapLog
 
-AzureRolePermsFile=$HOME/.Azure/PMCExampleAzureRole.json
+AzureRolePermsFile=~/.Azure/PMCExampleAzureRole.json
 
 # Install nodejs and npm if they aren't installed
 
@@ -58,7 +58,7 @@ fi
 echo "Logging into Azure."
 echo
 
-while [ -z $Username ]; 
+while [ -z $Username  ]; 
 do
     read -p "Enter your Azure username : " Username
 done
@@ -76,23 +76,34 @@ echo "Need to create a ParkMyCloud application in your subscription."
 echo "Here's the catch: It must be unique. "
 echo
 
-while [ -z $AppName ]; 
+while [ -z $AppName  ]; 
 do
     read -p "What do you want to call it? (e.g., ParkMyCloud Azure Dev): " AppName
 done
 
+AzurePemFile=~/.Azure/$AppName.pem
 
-while [ -z $AppPwd ]; 
-do
-    echo "Enter password for your password: " 
-    read -s AppPwd
-done
+# Create App API Key
+
+openssl req -x509 -days 3650 -newkey rsa:2048 -out cert.pem -nodes -subj "/CN=$AppName" >>  /dev/null 2>&1
+cat ./privkey.pem cert.pem > $AzurePemFile
+rm -f ./privkey.pem cert.pem
+
+AzureCert=`awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/' $AzurePemFile | grep -v BEGIN | grep -v END` 
+ThumbPrint=`openssl x509 -in "$AppName.pem" -fingerprint -noout | sed 's/SHA1 Fingerprint=//g'  | sed 's/://g'`
+
+AppKey= 
+
 
 # Create App
+# Not sure how key is generated
 # Can have -p <password> and provide password, but never get the key back
+# Can use --cred-value <cred-value>, but not sure what goes here? 
+# What the heck does "the value of the "asymmetric" credential type. It represents the base 64 encoded certificate" mean?
 
 # Need proper enddate
-azure ad app create -n "$AppName" -m "https://console.parkmycloud.com" -i "https://$AppName-not-used" -p "$AppPwd" > $AzureAppLog
+# azure ad app create -n "$AppName" -m "https://console.parkmycloud.com" -i "https://notused" --cert-value "$AzureCert" --end-date "12/31/2299" > $AzureAppLog
+azure ad app create -n "$AppName" -m "https://console.parkmycloud.com" -i "https://notused" --cert-value "$AzureCert" > $AzureAppLog
 
 AppID=`grep AppId $AzureAppLog | awk -F": " '{print $3}' | xargs`
 
@@ -104,7 +115,7 @@ echo "Created service principal for application."
 echo 
 
 ServicePrincipalID=`grep Id $AzureServicePrincipalLog | awk -F": " '{print $3}' | xargs`
-ServicePrincipalName=`grep  -A1 -P 'Names' ~/.Azure/PMCAzureServicePrincipalLog | awk -F: '{print $2}' | grep -v Names | xargs`
+
 
 # Create custom role with limited permissions
 # Generate permissions file
@@ -139,19 +150,13 @@ echo
 
 RoleID=`grep Id $AzureRoleLog | awk -F": " '{print $3}' | xargs`
 
-# 
-while [ -z "$SP_Present" ];
-do
-    SP_Present=`azure ad sp list | grep $ServicePrincipalID`
-    echo "$SP_Present"
-    echo "Waiting on Service Principal to show up in AD"
-    sleep 30
-done
+# Azure is S-L-O-W-W-W! Buy some time until it finishes generating the role before trying to assign it
+sleep 15
+
 
 # Assign role to application service principal
-# sh ./map_role.sh
 
-azure role assignment create "$ServicePrincipalID" --roleId "$RoleID"  --scope "/subscriptions/$SubscriptionID" > $AzureRoleMapLog
+azure role assignment create --objectId $ServicePrincipalID  --roleId $RoleID  --scope "/subscriptions/$SubscriptionID" > $AzureRoleMapLog
 
 echo "Role has been mapped to service principal for application."
 echo 
@@ -160,7 +165,7 @@ echo
 #   Subscription ID
 #   Tenant ID
 #   App ID (Client ID)
-#   App API Access Key
+#   App Access Key (Client Secret Key)
 
 echo "Your subscription ID is $SubscriptionID"
 echo
@@ -168,12 +173,8 @@ echo "Your Tenant ID is $TenantID"
 echo 
 echo "Your App ID is $AppID"
 echo
-echo "Your API Access Key is $AppPwd"
+echo "Your API Access Key is $AppKey"
 echo 
 echo "Enter these on the Azure credential page in ParkMyCloud."
 echo 
-
-echo "If you want to login interactively with this service principal, enter the following from the CLI:"
-echo 
-echo "azure login -u $ServicePrincipalName --service-principal --tenant $TenantID"
 
--- get_azure_cred/test_cert.sh
+++ get_azure_cred/test_cert.sh
@@ -5,15 +5,20 @@ do
     read -p "What do you want to call it? (e.g., ParkMyCloud Azure Dev): " AppName
 done
 
+AzurePemFile=$HOME/.Azure/$AppName.pem
+
+# Create App API Key
 
 openssl req -x509 -days 3650 -newkey rsa:2048 -out cert.pem -nodes -subj "/CN=$AppName" >>  /dev/null 2>&1
-cat ./privkey.pem cert.pem > $AppName.pem
+cat ./privkey.pem cert.pem > $AzurePemFile
 rm -f ./privkey.pem cert.pem
 
-AzureCert=` awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/' $AppName.pem | grep -v BEGIN | grep -v END` 
-
+AzureCert=`awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/' $AzurePemFile | grep -v BEGIN | grep -v END` 
+ThumbPrint=`openssl x509 -in "$AzurePemFile" -fingerprint -noout | sed 's/SHA1 Fingerprint=//g'  | sed 's/://g'`
 
 # AzureCert=`sed -n '/^-----BEGIN CERTIFICATE-----$/,/^-----END CERTIFICATE-----$/p' ./$AppName.pem`
 # sed -n '/-----BEGIN CERTIFICATE----- /,/ -----END CERTIFICATE-----/p' ./$AppName.pem
 
-echo $AzureCert
+echo "Cert: $AzureCert"
+echo
+echo "Fingerprint: $ThumbPrint"
